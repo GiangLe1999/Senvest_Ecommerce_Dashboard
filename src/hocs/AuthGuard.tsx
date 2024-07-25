4e45159cc83116c3e0b6240911c48f326e24993c
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-07-25 10:53:32 +0700

    Turn on auth guard

--- src/hocs/AuthGuard.tsx
+++ src/hocs/AuthGuard.tsx
@@ -1,15 +1,19 @@
 // Third-party Imports
-import { getServerSession } from 'next-auth'
+import { getServerSession } from "next-auth";
 
 // Type Imports
-import type { Locale } from '@configs/i18n'
-import type { ChildrenType } from '@core/types'
+import type { Locale } from "@configs/i18n";
+import type { ChildrenType } from "@core/types";
 
 // Component Imports
-import AuthRedirect from '@/components/AuthRedirect'
+import AuthRedirect from "@/components/AuthRedirect";
+import { authOptions } from "@/libs/auth";
 
-export default async function AuthGuard({ children, locale }: ChildrenType & { locale: Locale }) {
-  const session = await getServerSession()
+export default async function AuthGuard({
+  children,
+  locale,
+}: ChildrenType & { locale: Locale }) {
+  const session = await getServerSession(authOptions);
 
-  return <>{session ? children : <AuthRedirect lang={locale} />}</>
+  return <>{session ? children : <AuthRedirect lang={locale} />}</>;
 }
