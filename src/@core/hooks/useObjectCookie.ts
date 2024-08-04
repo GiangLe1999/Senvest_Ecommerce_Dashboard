99a7f218a5422ee79bbba0cd208bd36979996e42
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-08-04 22:08:41 +0700

    Update setting cookies forever

--- src/@core/hooks/useObjectCookie.ts
+++ src/@core/hooks/useObjectCookie.ts
@@ -1,19 +1,30 @@
 // React Imports
-import { useMemo } from 'react'
+import { useMemo } from "react";
 
 // Third-party Imports
-import { useCookie } from 'react-use'
+import { useCookie } from "react-use";
+import Cookies from "js-cookie";
 
-export const useObjectCookie = <T>(key: string, fallback?: T | null): [T, (newVal: T) => void] => {
+export const useObjectCookie = <T>(
+  key: string,
+  fallback?: T | null,
+): [T, (newVal: T) => void] => {
   // Hooks
-  const [valStr, updateCookie] = useCookie(key)
+  const [valStr, updateCookie] = useCookie(key);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const value = useMemo<T>(() => (valStr ? JSON.parse(valStr) : fallback), [valStr])
+  const value = useMemo<T>(
+    () => (valStr ? JSON.parse(valStr) : fallback),
+    [valStr],
+  );
 
   const updateValue = (newVal: T) => {
-    updateCookie(JSON.stringify(newVal))
-  }
+    // Update the cookie with js-cookie to set a long expiration date
+    Cookies.set(key, JSON.stringify(newVal), { expires: 365 * 10, path: "/" });
 
-  return [value, updateValue]
-}
+    // Update the state managed by useCookie
+    updateCookie(JSON.stringify(newVal));
+  };
+
+  return [value, updateValue];
+};
