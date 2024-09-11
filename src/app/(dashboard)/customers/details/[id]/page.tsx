0a74a79a9f86e5a3d8316265914adb6f4e31fb96
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-09-11 08:43:11 +0700

    Update customer detail page

--- src/app/(dashboard)/customers/details/[id]/page.tsx
+++ src/app/(dashboard)/customers/details/[id]/page.tsx
@@ -1,14 +1,18 @@
-import CustomerDetails from '@/views/apps/ecommerce/customers/details'
+// import CustomerDetails from '@/views/apps/ecommerce/customers/details'
 
 // Data Imports
-import {  getUserById } from '@/app/server/actions'
+// import {  getUserById } from '@/app/server/actions'
 
-const CustomerDetailsPage = async ({ params }: { params: { id: string } }) => {
+const CustomerDetailsPage = async (
+  // { params }: { params: { id: string } }
+) => {
   // Vars
-  const data = await getUserById(params.id)
+  // const data = await getUserById(params.id)
 
 
-  return <CustomerDetails customerData={data?.user} customerAddresses={data?.userAddresses} customerOrders={data?.userOrders} customerId={params.id} /> 
+  return <></> 
+
+  // <CustomerDetails customerData={data?.user} customerAddresses={data?.userAddresses} customerOrders={data?.userOrders} customerId={params.id} /> 
 }
 
 export default CustomerDetailsPage
