3b5dc00db73f9637d67bd3c09c707eeeb7fe101a
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-09-11 09:05:52 +0700

    Update dashboard

--- src/views/apps/ecommerce/orders/list/OrderListTable.tsx
+++ src/views/apps/ecommerce/orders/list/OrderListTable.tsx
@@ -135,7 +135,7 @@ const columnHelper = createColumnHelper<ECommerceOrderTypeWithAction>();
 const OrderListTable = ({ orderData }: { orderData?: OrderType[] }) => {
   // States
   const [rowSelection, setRowSelection] = useState({});
-  /* eslint-disable @typescript-eslint/no-unused-vars */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [data, setData] = useState(...[orderData]);
   const [globalFilter, setGlobalFilter] = useState("");
 
